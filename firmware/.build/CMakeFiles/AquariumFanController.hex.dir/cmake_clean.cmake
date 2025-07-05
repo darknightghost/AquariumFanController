969e54794718356270bcf6627ec5e88bff22f3b7
wang_siyuan <darknightghost@gmail.com>
2025-07-06 00:15:00 +0800

    UTC:2025-07-05 16:15:00 :

--- firmware/.build/CMakeFiles/AquariumFanController.hex.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "/home/darknightghost/projects/AquariumFanController/bin/AquariumFanController.hex"
-  "CMakeFiles/AquariumFanController.hex"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/AquariumFanController.hex.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- firmware/.build/CMakeFiles/AquariumFanController.hex.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for AquariumFanController.hex.
-# This may be replaced when dependencies are built.
